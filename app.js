555b4c18325f85bca6ae74b0271901fe795a7e0d
Marcelo Valdez <marcelogvz27@gmail.com>
2025-12-10 06:09:02 -0300

    termine mi app

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require("express");
-const app =  express();
-const puerto = 3000;
+const app = express();
+const PORT = 3000;
 
 // Middleware para leer datos de formularios y JSON
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 // Configurar EJS como motor de plantillas
 app.set("view engine", "ejs");
 
 // Carpeta de archivos públicos (CSS/JS)
-app.use(express.static("publico"))
+app.use(express.static("public"));
 
-//ruta principal
+// Ruta principal
 app.get("/", (req, res) => {
-    res.send(" servidor Express corriendo!");
+    res.send("Servidor Express corriendo!");
 });
 
-//importar rutas de temas
-const temasrutas = require("//rutas/temasrutas");
+// Importar rutas de temas
+const temasrutas = require("./rutas/temasrutas");
 app.use("/temas", temasrutas);
 
-//iniciar servidor
-app.listem(puerto, () => console.log(`Servidor en http://localhost:${puerto}`));
+// Iniciar servidor
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));

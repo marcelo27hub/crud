1dac0cf6ac05d6642f6bc6c6d044b769349aaf72
Marcelo <marcelogvz27@gmail.com>
2026-01-11 09:36:43 -0300

    tube problemas con git

--- rutas/temasrutas.js
+++ rutas/temasrutas.js
@@ -2,14 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {mostrarTemas, crearTemas, votarTemas} = require("../controladores/temascontroladores");
 
+const {mostrarTemas, crearTemas} = require("../controladores/temascontroladores");
+
 //mostrar los temas
 router.get("/", mostrarTemas);
 
 //crear temas
+
 router.post("/crear", crearTemas);
 
 //votar temas
 router.put("/votar", votarTemas);
+
+
+router.post("/crear", crearTemas)
 
 
 module.exports = router;

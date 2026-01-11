ca922ed2a6736c571276d42ddfc1a44122be3a79
Marcelo <marcelogvz27@gmail.com>
2026-01-11 04:36:46 -0300

    servidor listo

---
+++ app.js
@@ -0,0 +1,25 @@
+//prepaaramos el servidor
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+//middleware para leer datos de formularios y html
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//usar ejs como motor de plantillas
+app.set("view engine", "ejs");
+
+//carpetas de archivos publicos
+app.use(express.static("public"));
+
+//ruta principal 
+app.get("/", (req, res) =>{
+    res.send("servidor express corriendo!");
+});
+
+
+
+//iniciar servidor
+app.listen(PORT, () => console.log(`servidor activo en http://localhost:${PORT}`));

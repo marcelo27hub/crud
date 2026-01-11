6c40402a17f8c505afc6b69f5d453be78ccd5771
Marcelo <marcelogvz27@gmail.com>
2026-01-11 10:09:20 -0300

    ruta para eliminar enlace

--- rutas/temasrutas.js
+++ rutas/temasrutas.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {mostrarTemas, crearTemas, votarTemas, eliminarTemas, crearEnlace, votarEnlace} = require("../controladores/temascontroladores");
+const {mostrarTemas, crearTemas, votarTemas, eliminarTemas, crearEnlace, votarEnlace, eliminarEnlace} = require("../controladores/temascontroladores");
 
 //mostrar los temas
 router.get("/", mostrarTemas);
@@ -17,6 +17,6 @@ router.delete("/borrar/:id", eliminarTemas);
 
 //crear enlace, votar enlace, eliminar enlace
 router.post("/:id/enlaces", crearEnlace);
-router.put("/id/enlaces/:enlaceId/votar", votarEnlace);
-
+router.put("/:id/enlaces/:enlaceId/votar", votarEnlace);
+router.delete(":/id/enlaces/:enlaceId", eliminarEnlace)
 module.exports = router;
